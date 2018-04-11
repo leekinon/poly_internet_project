7959b9bd7f091f051eaacca81ca48b399ffe2531
winson <winson0801@gmail.com>
2018-04-11 19:27:09 +0800

    just testing

--- models/user.js
+++ models/user.js
@@ -8,11 +8,13 @@ var userSchema = new Schema({
 });
 
 userSchema.methods.encryptPassword = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);  
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);  
+  return bcrypt.compareSync(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
+//test
+//test1
